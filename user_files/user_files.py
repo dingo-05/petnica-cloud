0115c678dabfd8a5e5dd1991244de384622f2205
Mladen Djordjevic <djordjevic.mladenn@gmail.com>
2023-08-28 21:42:00 +0200

    RADI

--- user_files/user_files.py
+++ user_files/user_files.py
@@ -1,14 +1,14 @@
 import os
 import subprocess
 import shutil
-
-    server_path = fr'C:\Mladen\Piton\Users\{username}'
-
+    
 def send_files(username):
+    server_path = fr'C:\Mladen\Piton\Users\{username}'
     folder_path = input("Unesi lokaciju fajla koji zelis da uploadujes")
     shutil.copy(folder_path, server_path)
 
 def create_user_folder(username):
+    server_path = fr'C:\Mladen\Piton\Users\{username}'
     
     # Create user's folder if it doesn't exist
     if not os.path.exists(server_path):
@@ -28,8 +28,9 @@ def set_folder_permissions(username):
     subprocess.run(['icacls', server_path, f'/deny', f'{username}:(OI)(CI)RX'])
     
     print(f"Permissions set for user '{username}'")
-
-    subprocess.run(['explorer',server_path])
+    
+    # Open user's folder in File Explorer
+    subprocess.run(['explorer', server_path])
 
 if __name__ == "__main__":
     username = input("Enter the username: ")
@@ -37,4 +38,3 @@ if __name__ == "__main__":
     create_user_folder(username)
     set_folder_permissions(username)
     send_files(username)
-
