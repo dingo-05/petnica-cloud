2a066001d7a8096c01af83a700b5057fd6a5d64a
Mladen Djordjevic <djordjevic.mladenn@gmail.com>
2023-08-28 18:22:14 +0200

    Kopiranje fajla

--- user_files/user_files.py
+++ user_files/user_files.py
@@ -1,32 +1,40 @@
 import os
 import subprocess
+import shutil
+
+    server_path = fr'C:\Mladen\Piton\Users\{username}'
+
+def send_files(username):
+    folder_path = input("Unesi lokaciju fajla koji zelis da uploadujes")
+    shutil.copy(folder_path, server_path)
 
 def create_user_folder(username):
-    folder_path = fr'C:\Mladen\Piton\parent\{username}'
     
     # Create user's folder if it doesn't exist
-    if not os.path.exists(folder_path):
-        os.mkdir(folder_path)
+    if not os.path.exists(server_path):
+        os.mkdir(server_path)
         print(f"Folder created for user '{username}'")
 
 def set_folder_permissions(username):
-    folder_path = fr'C:\Mladen\Piton\parent\{username}'
+    server_path = fr'C:\Mladen\Piton\Users\{username}'
     
     # Set permissions using icacls command
-    subprocess.run(['icacls', folder_path, '/inheritance:r'])
+    subprocess.run(['icacls', server_path, '/inheritance:r'])
     
     # Grant user permission to their folder
-    subprocess.run(['icacls', folder_path, f'/grant:r', f'{username}:(OI)(CI)F'])
+    subprocess.run(['icacls', server_path, f'/grant:r', f'{username}:(OI)(CI)F'])
     
     # Deny user access to parent folder
-    subprocess.run(['icacls', folder_path, f'/deny', f'{username}:(OI)(CI)RX'])
+    subprocess.run(['icacls', server_path, f'/deny', f'{username}:(OI)(CI)RX'])
     
     print(f"Permissions set for user '{username}'")
 
-    subprocess.run(['explorer',folder_path])
+    subprocess.run(['explorer',server_path])
 
 if __name__ == "__main__":
     username = input("Enter the username: ")
     
     create_user_folder(username)
     set_folder_permissions(username)
+    send_files(username)
+
